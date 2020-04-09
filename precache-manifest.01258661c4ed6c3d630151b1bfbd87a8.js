8dcf4099ef64059aa258994962832e3257d032e3
Swapnil Pakhare <swapchamps@gmail.com>
2020-04-09 13:22:14 +0530

    Updates

---
+++ precache-manifest.01258661c4ed6c3d630151b1bfbd87a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648e36933b4ef9234890946b45aeb9d3",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "1361a7a14d7889d755a9",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "134def81b153d3636e4f",
+    "url": "/react-gh-pages/static/js/2.3c16eec8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.3c16eec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1361a7a14d7889d755a9",
+    "url": "/react-gh-pages/static/js/main.fd94b66a.chunk.js"
+  },
+  {
+    "revision": "b15fab85dea6ea389910",
+    "url": "/react-gh-pages/static/js/runtime-main.b1e97c2c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
